e176cecc55e3ec0de279e312eea152676fb17623
Muskaan Singh <muskaansingh235@gmail.com>
2021-04-07 17:03:42 +0530

    added cards and styles.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import './App.css';
-import {Typography, CssBaseline, AppBar, Toolbar, Container, Grid, Button} from '@material-ui/core';
+import {Typography, CssBaseline, AppBar, Toolbar, Container, Grid, Button, Card, CardMedia, CardContent, CardActions} from '@material-ui/core';
 import {PhotoCamera} from '@material-ui/icons';
+import useStyles from './styles';
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 function App() {
+  const classes= useStyles();
   return (
     <>
       <CssBaseline/>
       <AppBar position="relative">
           <Toolbar>
-            <PhotoCamera/>
+            <PhotoCamera className={classes.icon}/>
             <Typography variant="h5">AlbumLayout</Typography>
           </Toolbar>
       </AppBar>
 
       <main>
         <div>
-          <Container maxWidth="sm">
+          <Container maxWidth="sm" className={classes.container}>
             <Typography variant="h1" align="center" color="textPrimary" gutterBottom>
                 Photo Album
             </Typography>
@@ -24,7 +28,7 @@ function App() {
               Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
               when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
             </Typography>
-            <div>
+            <div className={classes.buttons}>
               <Grid container spacing={2} justify="center">
                   <Grid item>
                     <Button variant="contained" color="primary">  
@@ -40,6 +44,31 @@ function App() {
             </div>
           </Container>
         </div>
+
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+          {cards.map((card) => (
+            <Grid item key={card} xs={12} sm={6} md={4}>
+                  <Card className={classes.card}>
+                      <CardMedia 
+                          className={classes.cardMedia}
+                          image="https://source.unsplash.com/random"
+                          title="Image Title"
+                      />
+                      <CardContent className={classes.cardContent}>
+                          <Typography gutterBottom variant="h4">Heading!</Typography>
+                          <Typography paragraph>hbdfajdflh  eufhjfjle ugfhuwfh fgLUWEFJBJENDJN</Typography>
+                      </CardContent>
+                      <CardActions>
+                        <Button size="small" color="primary">View</Button>
+                        <Button size="small" color="primary">Edit</Button>
+                      </CardActions>
+                  </Card>
+              </Grid>
+          ))}    
+          </Grid>
+        </Container>     
+      
       </main>
     </>
   );
---
+++ src/styles.js
@@ -0,0 +1,30 @@
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ( {
+  container: {
+    backgroundColor: "#F0F0F0",
+    marginTop:"100px"
+  },
+  icon:{
+    marginRight: "20px"
+  },
+  buttons:{
+    marginTop:"40px",
+  },
+  cardGrid:{
+    padding: "20px 0"
+  },
+  card:{
+    height:"100%",
+    display:"flex",
+    flexDirection:"column"
+  },
+  cardMedia: {
+    paddingTop:"56.25%" // 16:9
+  },
+  cardContent: {
+    flexGrow:1
+  }
+}));
+
+export default useStyles;
