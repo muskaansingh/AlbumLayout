147e56d512e99ced780c0b1aa4d96a61c5f75c9a
Muskaan Singh <muskaansingh235@gmail.com>
2021-04-07 15:52:23 +0530

    added appbar, typography and buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import './App.css';
+import {Typography, CssBaseline, AppBar, Toolbar, Container, Grid, Button} from '@material-ui/core';
+import {PhotoCamera} from '@material-ui/icons';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <CssBaseline/>
+      <AppBar position="relative">
+          <Toolbar>
+            <PhotoCamera/>
+            <Typography variant="h5">AlbumLayout</Typography>
+          </Toolbar>
+      </AppBar>
+
+      <main>
+        <div>
+          <Container maxWidth="sm">
+            <Typography variant="h1" align="center" color="textPrimary" gutterBottom>
+                Photo Album
+            </Typography>
+            <Typography variant="h6" align="center" color="textSecondary" paragraph>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+              when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+            </Typography>
+            <div>
+              <Grid container spacing={2} justify="center">
+                  <Grid item>
+                    <Button variant="contained" color="primary">  
+                        See Photos!
+                    </Button>
+                  </Grid>
+                  <Grid item>
+                    <Button variant="outlined" color="primary">
+                        See Videos!
+                    </Button>
+                  </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+      </main>
+    </>
   );
 }
 
